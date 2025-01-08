138fd278659a96e743741c97166a3bf40164b8b5
Marwin2002 <musicbotid@gmail.com>
2025-01-08 18:30:02 +0530

    Update play.py

--- JioSavaan/utils/inline/play.py
+++ JioSavaan/utils/inline/play.py
@@ -33,54 +33,62 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "❖◈◈◈◈◈◈◈◈◈"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "◈❖◈◈◈◈◈◈◈◈"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "◈◈❖◈◈◈◈◈◈◈"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "◈◈◈❖◈◈◈◈◈◈"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "◈◈◈◈❖◈◈◈◈◈"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "◈◈◈◈◈❖◈◈◈◈"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "◈◈◈◈◈◈❖◈◈◈"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "◈◈◈◈◈◈◈❖◈◈"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "◈◈◈◈◈◈◈◈❖◈"
     else:
-        bar = "—————————◉"
+        bar = "◈◈◈◈◈◈◈◈◈❖"
+       
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
+                [   InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
+                callback_data="GetTimer"
+                )
+         ],
+         [
+            InlineKeyboardButton(text="ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}")
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-    ]
+        [
+            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],
+        [
+
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ]
     return buttons
 
 
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+            InlineKeyboardButton(text="ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}")
+         ],
+         [
+            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ", callback_data=f"ADMIN Replay|{chat_id}"),
+         ],
+         [
+
+     InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -90,11 +98,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -126,7 +134,7 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
 
 
 def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
-    query = f"{query[:200]}"
+    query = f"{query[:20]}"
     buttons = [
         [
             InlineKeyboardButton(
