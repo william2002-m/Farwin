05ce12c5a27fd40d88cd1eb5ccd31c8894ae1212
Marwin2002 <musicbotid@gmail.com>
2025-01-11 22:29:25 +0530

    Update start.py

--- JioSavaan/utils/inline/start.py
+++ JioSavaan/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
