214d3801b2b61d3d58e97ea11edb3fddcc382f63
Marwin2002 <musicbotid@gmail.com>
2025-01-18 17:26:32 +0530

    Update Welcome.py

--- JioSavaan/plugins/tools/Welcome.py
+++ JioSavaan/plugins/tools/Welcome.py
@@ -120,8 +120,8 @@ async def greet_group(_, member: ChatMemberUpdated):
             member.chat.id,
             photo=welcomeimg,
             caption= f"""
-**
-ㅤㅤㅤ◦•●◉✿ ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ ✿◉●•◦
+
+ㅤ◦•●◉✿ ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ ✿◉●•◦
 ▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
 
 ● ɴᴀᴍᴇ ➥  {user.mention}
