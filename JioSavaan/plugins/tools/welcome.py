a01a6dc21d2ccfbe375154dba6d58541981920aa
Marwin2002 <musicbotid@gmail.com>
2025-01-20 21:46:32 +0530

    Update welcome.py

--- JioSavaan/plugins/tools/welcome.py
+++ JioSavaan/plugins/tools/welcome.py
@@ -90,7 +90,7 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("JioSavaan/assets/Wel2.png")
+    background = Image.open("JioSavaan/assets/20250120_214235.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((512, 512))
