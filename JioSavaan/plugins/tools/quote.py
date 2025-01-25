0f6502441accc0ec1ecce5467f0912627bc10acf
Markus04p <teambotid04@gmail.com>
2025-01-25 09:39:30 +0530

    Update quote.py

--- JioSavaan/plugins/tools/quote.py
+++ JioSavaan/plugins/tools/quote.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from PRINCEMUSIC import app
+from JioSavaan import app
 from httpx import AsyncClient, Timeout
 # -----------------------------------------------------------------
 fetch = AsyncClient(
