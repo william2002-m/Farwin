3ff88eb295793a24cb912a42528502f5ea535675
Block2002 <marwin2005op@gmail.com>
2025-02-01 11:11:37 +0530

    Update download_stream.py

--- JioSavaan/plugins/tools/download_stream.py
+++ JioSavaan/plugins/tools/download_stream.py
@@ -9,8 +9,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 
-from VIPMUSIC import app
-from VIPMUSIC.platforms.Youtube import cookies as cookie_txt_file
+from JioSavaan import app
+from JioSavaan.platforms.Youtube import cookies as cookie_txt_file
 
 # Define a dictionary to track the last query timestamp for each user
 user_last_CallbackQuery_time = {}
