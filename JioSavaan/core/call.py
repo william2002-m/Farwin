e627291c95d1ea5982bd43c8ebf7f4a10adb7a4c
Block2002 <marwin2005op@gmail.com>
2025-02-01 13:28:37 +0530

    Update call.py

--- JioSavaan/core/call.py
+++ JioSavaan/core/call.py
@@ -561,18 +561,17 @@ class Call(PyTgCalls):
     async def ping(self):
         pings = []
         if config.STRING1:
-            pings.append(self.one.ping)
+            pings.append(await self.one.ping)
         if config.STRING2:
-            pings.append(self.two.ping)
+            pings.append(await self.two.ping)
         if config.STRING3:
-            pings.append(self.three.ping)
+            pings.append(await self.three.ping)
         if config.STRING4:
-            pings.append(self.four.ping)
+            pings.append(await self.four.ping)
         if config.STRING5:
-            pings.append(self.five.ping)
+            pings.append(await self.five.ping)
         return str(round(sum(pings) / len(pings), 3))
-    
-    
+
     async def start(self):
         LOGGER(__name__).info("Starting PyTgCalls Client...\n")
         if config.STRING1:
@@ -586,39 +585,34 @@ class Call(PyTgCalls):
         if config.STRING5:
             await self.five.start()
 
-    
     async def decorators(self):
-        @self.one.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
-        @self.two.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
-        @self.three.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
-        @self.four.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
-        @self.five.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
-        
-
-        @self.one.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
-        @self.two.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
-        @self.three.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
-        @self.four.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
-        @self.five.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
-        
-            
-        @self.one.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
-        @self.two.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
-        @self.three.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
-        @self.four.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
-        @self.five.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
+        @self.one.on_kicked()
+        @self.two.on_kicked()
+        @self.three.on_kicked()
+        @self.four.on_kicked()
+        @self.five.on_kicked()
+        @self.one.on_closed_voice_chat()
+        @self.two.on_closed_voice_chat()
+        @self.three.on_closed_voice_chat()
+        @self.four.on_closed_voice_chat()
+        @self.five.on_closed_voice_chat()
+        @self.one.on_left()
+        @self.two.on_left()
+        @self.three.on_left()
+        @self.four.on_left()
+        @self.five.on_left()
         async def stream_services_handler(_, chat_id: int):
             await self.stop_stream(chat_id)
 
-        
-        @self.one.on_update(filters.stream_end)
-        @self.two.on_update(filters.stream_end)
-        @self.three.on_update(filters.stream_end)
-        @self.four.on_update(filters.stream_end)
-        @self.five.on_update(filters.stream_end)
+        @self.one.on_stream_end()
+        @self.two.on_stream_end()
+        @self.three.on_stream_end()
+        @self.four.on_stream_end()
+        @self.five.on_stream_end()
         async def stream_end_handler1(client, update: Update):
             if not isinstance(update, StreamAudioEnded):
                 return
             await self.change_stream(client, update.chat_id)
+
 
 Anony = Call()
