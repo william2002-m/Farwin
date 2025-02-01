64b706e89ae46474b9950790b307962566ae867c
Block2002 <marwin2005op@gmail.com>
2025-02-01 11:59:44 +0530

    Update bot.py

--- JioSavaan/core/bot.py
+++ JioSavaan/core/bot.py
@@ -14,9 +14,6 @@ class Anony(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
         )
 
     async def start(self):
