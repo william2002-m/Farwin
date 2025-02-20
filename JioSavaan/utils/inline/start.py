17b4085e5a085ef425183e68746c2937e6348018
Block2002 <marwin2005op@gmail.com>
2025-02-20 23:56:15 +0530

    Update start.py

--- JioSavaan/utils/inline/start.py
+++ JioSavaan/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], url=f"https://t.me/Dev_Otsutsuki"),
+            InlineKeyboardButton(text=_["S_B_5"], url=f"https://t.me/Itzzz_cupcake"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
